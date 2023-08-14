b49ede3afcc47cfe65e0a03d2845f7e309c551db
Roman <jrj80mail@gmail.com>
2023-08-14 12:28:07 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 //?   npm i slim-select
 //?   npm i notiflix
 //?   npm install axios // поки не використовую
+
+import SlimSelect from 'slim-select';
+import 'slim-select/dist/slimselect.css';
+import { fetchBreeds, fetchCatByBreed } from './cat-api';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+//* Вибір DOM-елементів ( document.querySelector() )
+const $selectWrapper = document.querySelector('.select-wrapper');
+const $select = document.querySelector('.breed-select');
+const $card = document.querySelector('div.cat-info');
+const $loader = document.querySelector('.loader');
