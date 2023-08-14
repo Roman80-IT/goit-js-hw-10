1e1f9ce1bd758f29b6e7bb85c8d8270da094d955
Roman <jrj80mail@gmail.com>
2023-08-14 12:32:21 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -12,3 +12,14 @@ const $selectWrapper = document.querySelector('.select-wrapper');
 const $select = document.querySelector('.breed-select');
 const $card = document.querySelector('div.cat-info');
 const $loader = document.querySelector('.loader');
+
+//* Запит до API для отримання списку порід котів
+fetchBreeds()
+  .then(breeds => {
+    renderBreeds(breeds); //* Рендеринг списку порід у селекті
+    $selectWrapper.classList.remove('is-hidden'); //* Показуємо обгортку для вибору пород
+    $loader.classList.add('is-hidden'); //* Ховаємо спіннер завантаження
+  })
+  .catch(() =>
+    Notify.failure('Oops! Something went wrong! Try to reload the page!')
+  );
